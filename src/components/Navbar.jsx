0a370cd88af840ab77ef55983676fdcefa404a62
mendi yakobov <mendyy2000@gmail.com>
2024-09-12 12:52:40 +0300

    beafore notification sign

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,24 @@ import User from './User'
 import SelectBox from './SelectBox'
 
 const Navbar = () => {
-
     const str = "פיקוד ההכשרות והאימונים / תומר / לובי כניסה"
-
     const parts = str.split(' / ');
 
     return (
         <nav className="py-3 border-b-2 border-b-border px-10 flex items-center justify-between ">
 
-            <div className="">
-                {parts[0]} / {parts[1]} / <span className='text-primary'>{parts[2]}</span>
+            <div>
+                {parts.slice(0, parts.length - 1).map((part, index) => (
+                    <span key={index}>{part} / </span>
+                ))}
+                <span className='text-primary'>{parts[parts.length - 1]}</span>
             </div>
 
             <div className='flex items-center gap-8'>
                 <SelectBox />
                 <User />
             </div>
-            
+
         </nav>
     )
 }
--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -8,11 +8,11 @@ const SelectBox = () => {
     return (
         <div className="w-auto flex  items-center">
             <IoMdNotifications className='text-3xl text-[#94a3b8]' />
-                <select name="box" id="box" className="px-5 block w-full border-blue-500 rounded-lg text-lg leading-tight focus:outline-none focus:border-blue-700 focus:ring-1 focus:ring-blue-700">
-                    {options.map(option => (
-                        <option key={option} value={option}>{option}</option>
-                    ))}
-                </select>
+            <select name="box" id="box" className="px-5 block w-full border-blue-500 rounded-lg text-lg leading-tight focus:outline-none focus:border-blue-700 focus:ring-1 focus:ring-blue-700">
+                {options.map(option => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
+            </select>
         </div>
     );
 }
