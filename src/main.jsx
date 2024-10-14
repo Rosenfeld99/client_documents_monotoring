b10ff23ba8461c81f94d85b78a5c795e82858c74
nehoray <nehorayatia1@gmail.com>
2024-10-14 10:44:38 +0300

    marge

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <React.Fragment>
+  <React.Fragment> 
     <App />
   </React.Fragment>,
 )
